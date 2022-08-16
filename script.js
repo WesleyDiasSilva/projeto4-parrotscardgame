59455ec9cadde90fc178fce6fedefa777907b838
Wesley Dias <wesley.dias.dev@gmail.com>
2022-08-16 13:07:54 -0300

    Finalizei a parte do prompt do usuário para escolher quantas cartas quer no jogo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('teste')
+let quantidadeCartas = prompt('Por favor, digite aqui apenas a quantidade de cartas com que jogar! \nIMPORTANTE: Escolha de 4 a 14 cartas, e sempre números pares!')
+quantidadeCartas = Number(quantidadeCartas)
+
+while(quantidadeCartas == null || quantidadeCartas < 4 || quantidadeCartas > 14 || isNaN(quantidadeCartas) || quantidadeCartas % 2 === 1){
+  quantidadeCartas = prompt('Por favor, digite aqui a quantidade de cartas com que jogar! \nIMPORTANTE: Escolha de 4 a 14 cartas, e sempre números pares!')
+}
+
+console.log(quantidadeCartas)
